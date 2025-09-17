e07afc9a8c45b13f40abd041da60d2a7f62d496f
Hero-mario <123155436+Sunit46x@users.noreply.github.com>
2025-09-17 23:31:53 +0530

    Initial commit: added CategoryList and CategoryPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from 'react-redux'
-import Store from './app/Store'
-import { Toaster } from 'react-hot-toast'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CategoryPage from './components/CategoryPage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import Store from "./app/Store";
+import App from "./App";
+import CategoryList from "./components/CategoryList";
+import CategoryPage from "./components/CategoryPage";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
       <Toaster position="top-center" reverseOrder={false} />
-      {/* ✅ Add basename for GitHub Pages */}
       <Router basename="/Layerflex">
         <Routes>
-          <Route path="/" element={<App />} />
+          {/* Home page showing all categories */}
+          <Route path="/" element={<CategoryList />} />
+
+          {/* Dynamic category page */}
           <Route path="/category/:categoryName" element={<CategoryPage />} />
-          <Route path="*" element={<App />} />
+
+          {/* Fallback for unknown routes */}
+          <Route path="*" element={<CategoryList />} />
         </Routes>
       </Router>
-
     </Provider>
   </React.StrictMode>
-)
+);
