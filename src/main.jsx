67bed78a120ad821a608a71fa5a01757c4000b44
Hero-mario <123155436+Sunit46x@users.noreply.github.com>
2025-09-18 11:23:46 +0530

    day1 push2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' // Tailwind CSS entry
-import { Provider } from 'react-redux'
-import Store from './app/Store'
-import { Toaster } from 'react-hot-toast'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CategoryPage from './components/CategoryPage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Tailwind CSS entry
+import { Provider } from "react-redux";
+import Store from "./app/Store";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CategoryPage from "./components/CategoryPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
       <Toaster position="top-center" reverseOrder={false} />
@@ -18,11 +18,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/category/:categoryName" element={<CategoryPage />} />
-
+          
           {/* 👇 fallback to App if no route matches */}
           <Route path="*" element={<App />} />
         </Routes>
       </Router>
     </Provider>
   </React.StrictMode>
-)
+);
